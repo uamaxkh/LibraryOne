403d0f0cdf179cce056e9f4444650e390ca69fde
uamaxkh <uamaxkh@gmail.com>
2019-07-02 10:13:12 +0300

    Changed RegisteredUsers style -> table

---
+++ Library/DBLib/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DBLib
+{
+    /// <summary>
+    /// Writes the error messages to log file
+    /// </summary>
+    public static class Logger
+    {
+        public static void Log(string message, string serverPath)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(serverPath, "libLog.txt"), true))
+            {
+                outputFile.WriteLine(message);
+            }
+        }
+        public static void Log(List<string> messages, string serverPath)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(serverPath, "libLog.txt"), true))
+            {
+                foreach(var massage in messages)
+                {
+                    outputFile.WriteLine(massage);
+                }
+            }
+        }
+    }
+}
--- Library/Library/Controllers/ErrorController.cs
+++ Library/Library/Controllers/ErrorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DBLib.Models;
+using System.IO;
 
 namespace Library.Controllers
 {
@@ -17,11 +18,29 @@ namespace Library.Controllers
 
         public ActionResult Error(Exception ms)
         {
+            List<string> errMessage = new List<string>();
+
+            errMessage.Add("---SystemException [" + DateTime.Now + "] ---");
+            errMessage.Add("Message: " + ms.Message);
+            errMessage.Add("Data: " + ms.Data);
+            errMessage.Add("InnerException: " + ms.InnerException);
+            errMessage.Add("Source: " + ms.Source);
+            errMessage.Add("StackTrace: " + ms.StackTrace + "\n");
+
+            DBLib.Logger.Log(errMessage, Server.MapPath("~/Log/"));
             return View(ms);
         }
 
         public ActionResult ErrorExt(ExceptionExt ms)
         {
+            List<string> errMessage = new List<string>();
+
+            errMessage.Add("---ExceptionExt [" + DateTime.Now + "] ---");
+            errMessage.Add("Message: " + ms.Message);
+            errMessage.Add("MessageExt: " + ms.MessageExt);
+            errMessage.Add("AdditionalInfo: " + ms.AdditionalInfo + "\n");
+
+            DBLib.Logger.Log(errMessage, Server.MapPath("~/Log/"));
             return View(ms);
         }
     }
